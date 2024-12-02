e4478fac2debb7adb659843736380d99211979cb
jj-jinosko <jesscj@umich.edu>
2024-12-02 16:17:57 -0500

    setup initial api request/response handling and output to ejs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import express from "express"
+import express from "express";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
@@ -6,9 +7,25 @@ const port = 3000;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+// app.get("/", (req, res) => {
+//     res.render("index.ejs")
+// });
+
+app.get("/", async (req, res) => {
+    try {
+        let response = await axios("https://pokeapi.co/api/v2/pokemon/35");
+        console.log("name:", response.data.name);
+        res.render("index.ejs", {
+            name: response.data.name,
+        })
+    } catch {
+
+    }
 })
+
+// app.post("/new", (req, res) => {
+
+// })
 
 app.listen(port, () => {
     console.log(`application listening on port ${port}`)
